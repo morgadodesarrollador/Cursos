2bc91ef4a75c4028aec895369aff3dc0cfa7bf10
Morgado <morgadodesarrollador@gmail.com>
2021-11-22 08:10:44 +0100

    5.10-. Resolver Types - People - Modificando propiedades github y twitter

--- GQL-Angular/01-book-store-api/src/db/resolvers/types.ts
+++ GQL-Angular/01-book-store-api/src/db/resolvers/types.ts
@@ -4,7 +4,7 @@ import data from "../data";
 // resolvemos los tipos de datos de las uniones
 const typesResolvers: IResolvers = {
   DataBD: {
-    __resolveType(obj: { name: string, isbn: string }) {
+    __resolveType(obj: { name: string; isbn: string }) {
       // Only Author has a name field
       if (obj.name) {
         return "People";
@@ -17,35 +17,51 @@ const typesResolvers: IResolvers = {
     },
   },
   People: {
-   // booksBuy:  (root: object) => { //objeto completo id, name, books ...
-    booksBuy:  (root: {books: Array<string>}) => {
-        //console.log(root.books)
-        const result = data.books.filter(
-            (book) => root.books.indexOf(book.id) > -1 )
-        return result;
-    }
+    // booksBuy:  (root: object) => { //objeto completo id, name, books ...
+    booksBuy: (root: { books: Array<string> }) => {
+      //console.log(root.books)
+      const result = data.books.filter(
+        (book) => root.books.indexOf(book.id) > -1
+      );
+      return result;
+    },
+    website: (root: { website: string }) => {
+      return root.website === undefined ? "" : root.website;
+    },
+    twitter: (root: { twitter: string }) => {
+      return root.twitter === undefined
+        ? ""
+        : `https://twitter.com/${root.twitter}`;
+    },
+    github: (root: { github: string }) => {
+      return root.github === undefined
+        ? ""
+        : `https://github.com/${root.github}`;
+    },
   },
   Book: {
     // booksBuy:  (root: object) => { //objeto completo id, name, books ...
-    byPeoplesBuy:  (root: {id: string}) => {
-         //console.log(root.books)
-         const result = data.people.filter( //itera en cada people
-             (people) => people.books.indexOf(root.id) > -1 ) // si existe ese id en people.books
-         return result;
-     },
-     publishedDate: (root: { publishedDate: string}) => {
-       return root.publishedDate === undefined ? "?" : root.publishedDate
-     },
-     thumbnailUrl: (root: { thumbnailUrl: string}) => {
-      return root.thumbnailUrl === undefined ? "?" : root.thumbnailUrl
+    byPeoplesBuy: (root: { id: string }) => {
+      //console.log(root.books)
+      const result = data.people.filter(
+        //itera en cada people
+        (people) => people.books.indexOf(root.id) > -1
+      ); // si existe ese id en people.books
+      return result;
     },
-    shortDescription: (root: { shortDescription: string}) => {
-      return root.shortDescription === undefined ? "?" : root.shortDescription
+    publishedDate: (root: { publishedDate: string }) => {
+      return root.publishedDate === undefined ? "?" : root.publishedDate;
     },
-    longDescription: (root: { longDescription: string}) => {
-      return root.longDescription === undefined ? "?" : root.longDescription
-    }
-   }
+    thumbnailUrl: (root: { thumbnailUrl: string }) => {
+      return root.thumbnailUrl === undefined ? "?" : root.thumbnailUrl;
+    },
+    shortDescription: (root: { shortDescription: string }) => {
+      return root.shortDescription === undefined ? "?" : root.shortDescription;
+    },
+    longDescription: (root: { longDescription: string }) => {
+      return root.longDescription === undefined ? "?" : root.longDescription;
+    },
+  },
 };
 
 export default typesResolvers;
