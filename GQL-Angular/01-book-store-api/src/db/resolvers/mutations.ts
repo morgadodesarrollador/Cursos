207dd100e4a7dc360a4fc5ae16f14cd1b0e80047
Morgado <morgadodesarrollador@gmail.com>
2021-11-22 12:04:57 +0100

    5.13-.Mutation - Añadir Libro (Book)

--- GQL-Angular/01-book-store-api/src/db/resolvers/mutations.ts
+++ GQL-Angular/01-book-store-api/src/db/resolvers/mutations.ts
@@ -1,12 +1,22 @@
+import { IResult } from './../../interfaces/IResult';
 
 import { IResolvers } from "@graphql-tools/utils";
 import { IBook } from '../../interfaces/IBook';
+import data from '../data';
 
 const mutationsResolvers: IResolvers = {
     Mutation: { //tipo raiz
-        addBook: (_:void, args: {book: IBook}): boolean => {
+        addBook: (_:void, args: {book: IBook}): IResult => {
             console.log(args.book);
-            return true;
+            //generamos el id automático
+            const idValue = +data.books[data.books.length-1].id;
+            args.book.id = String(idValue);
+            (data.books as IBook[]).push(args.book);
+            return {
+                status: true,
+                message: `Libro con el título ${args.book.title} ha sido añadido correctamente`,
+                item: args.book
+            };
         }
         //especificamos todas las definiciones de mutaions.ts
         /*
