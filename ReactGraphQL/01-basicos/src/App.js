5eb7e7858d1db14d01ac27c8801e38ef4b0306c3
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2021-11-26 00:11:40 +0100

    React:01-Basicos: State

--- ReactGraphQL/01-basicos/src/App.js
+++ ReactGraphQL/01-basicos/src/App.js
@@ -1,8 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Producto from "./components/Producto";
 
 function App() {
+
+  //crear una lista de productos
+  //destructuring [state, función que ayuda a reescribir (agrega/elimina elementos del state]
+  const [ productos, saveProductos ] = useState([ // valores iniciales
+    { id: 1, nombre: 'camisa ReactJS', precio: 50 }, 
+    { id: 2, nombre: 'camisa NodeJS', precio: 30 },
+    { id: 2, nombre: 'camisa VueJS', precio: 40 },
+    { id: 2, nombre: 'camisa AngularJS', precio: 55 }
+  ]); 
+
   const fecha = new Date().getFullYear();
   return (
    // <div className="App">
@@ -11,6 +22,13 @@ function App() {
         titulo = "Tienda Virtual OnLine"
         numero= "20"    
       />
+      <h1>Listado de Productos</h1>
+      { productos.map( producto => (  // producto => () equivale a producto => { return ... }
+          <Producto 
+            key = { producto.id } //hay q pasar al componente una key unica
+            producto = { producto }
+          />
+      ))}
       <Footer
         fecha={fecha}
       />
