acaedb8b00369a83226fd723ec9bedb13b89db88
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2021-12-14 12:01:31 +0100

    Graphql-09-api-jwt: Resolver Query: login()

--- GQL-Angular/09-jwt-login-register-mongodb/src/db/resolvers/users/query/users.ts
+++ GQL-Angular/09-jwt-login-register-mongodb/src/db/resolvers/users/query/users.ts
@@ -1,13 +1,38 @@
 import { IResolvers } from "@graphql-tools/utils";
-import { IResult } from "../../../interfaces/IResult";
 import { IUser } from "../../../interfaces/IUser";
 import { Db } from "mongodb";
+import { IResult } from "../../../interfaces/IResult";
 const queryResolvers: IResolvers = {
     Query: {
         users: async(_: void, __:unknown, context: { db: Db }) : Promise <Array<IUser>> => {
             const users = await context.db.collection("users").find().toArray() as Array<IUser>; 
             console.log(users);
             return users;
+        },
+        login: async(_: void, args: { email: string, password: string }, context: { db: Db }): Promise<IResult> =>  {
+            console.log(args);
+            return await context.db.collection("users").findOne(
+                { email: args.email, password: args.password }
+            ).then( (userD) => {
+               // delete userD?._id;
+                console.log(userD);
+                if (!userD) {
+                    return {
+                        status: true,
+                        message: 'Usuario No encontrado'
+                    };
+                }
+                return {
+                    status: true,
+                    message: 'Usuario cargado',
+                    data: userD as IUser
+                };
+            }).catch( (error) => {
+                return {
+                    status: false,
+                    message: `error Usuario ${error} no cargado `,
+                };
+            });
         }
     },
 }
